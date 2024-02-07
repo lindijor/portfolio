4a01bdfedd4a06c2a458fdfd32678446fadb21e5
lindijor <jorgensenlindis@gmail.com>
2024-02-07 15:02:06 +0100

    Added poem generator to work section

--- src/Work.js
+++ src/Work.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Work.css";
 import dictionary from "./dictionary.png";
 import weather from "./weather-app.png";
+import poem from "./poem.png";
 
 export default function Work() {
   return (
@@ -44,6 +45,26 @@ export default function Work() {
         </div>
         <div className="image">
           <img src={dictionary} alt="dictionary" />
+        </div>
+      </div>
+      <div className="work-section">
+        <div>
+          <h3>AI poem generator</h3>
+          <p>
+            An AI poem generator built using HTML, an active API, advanced CSS,
+            and React.
+          </p>
+          <a
+            href="https://your-ai-poem-generator.netlify.app/"
+            className="btn"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Check it out! <i class="fa-solid fa-arrow-up-right-from-square"></i>
+          </a>
+        </div>
+        <div className="image">
+          <img src={poem} alt="poem generator" />
         </div>
       </div>
     </div>
